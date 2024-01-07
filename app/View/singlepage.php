79af5eba1a711599490f3a9c548fc6bc1acaf10a
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-07 22:52:12 +0100

    Single Pages been created

--- app/View/singlepage.php
+++ app/View/singlepage.php
@@ -16,7 +16,23 @@
     <?php
     include "../app/View/includes/navbar.php";
     ?>
-
+    <!-- Content Section -->
+    <section class="bg-white dark:bg-gray-900 pt-24">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:flex lg:flex-col lg:py-16 lg:px-6">
+            <div class="font-light text-center sm:text-lg dark:text-gray-400">
+                <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Here goes the title</h2>
+                <p class="text-black dark:text-gray-500 "> Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+                <p class="mt-8 text-left text-gray-500 dark:text-gray-700 mb-4">by : Zaiid @at ...</p>
+            </div>
+            <div class="mt-2 self-center">
+                <img class="     rounded-lg" src="<?= URL_DIR ?>public/assets/images/article.jpg" alt="office content 1">
+            </div>
+            <div class="mt-2 text-black dark:text-gray-300">
+                <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+            </div>
+        </div>
+    </section>
     <!-- Footer -->
     <?php include "../app/View/includes/footer.php"; ?>
 
