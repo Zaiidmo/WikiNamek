21d8cc24807dc40742964e2df9f60d78cce26659
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-07 19:20:10 +0100

    Creating single Page

---
+++ app/Controller/SinglepageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class SinglepageController
+{
+    public function index()
+    {
+        include '../app/View/singlepage.php';
+    }
+}
---
+++ app/View/singlepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WikiNamek</title>
+    <meta name="keywords" content="HTML, CSS, Youcode, tailwindCSS, Wikinamek,Wiki, Wikis">
+    <meta name="author" content="Zaiid Moumnii">
+    <meta name="description" content="WikiNamek .. Your Gateway to infinit Insights">
+    <link rel="stylesheet" href="public/assets/dist/output.css">
+    <link rel="shortcut icon" href="<?= URL_DIR ?>public/assets/images/favicon.png" type="image/x-icon">
+</head>
+
+<body class="bg-gray-300 dark:bg-gray-900 text-gray-900 font-poppins">
+    <?php
+    include "../app/View/includes/navbar.php";
+    ?>
+
+    <!-- Footer -->
+    <?php include "../app/View/includes/footer.php"; ?>
+
+
+
+</body>
+<script src="public/assets/js/navbar.js"></script>
+<script src="public/assets/js/theme.js"></script>
+
+</html>
