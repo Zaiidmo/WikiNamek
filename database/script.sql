db09a97932074e63ab07dec82b317b1afea64aed
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-15 12:07:34 +0100

    Adding SQL Script

--- database/script.sql
+++ database/script.sql
@@ -1,200 +1,160 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jan 08, 2024 at 02:57 PM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `wikinamek`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
+CREATE DATABASE `Wikinamek`;
+-- Create Categories Table
 CREATE TABLE `category` (
-  `id` int NOT NULL,
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tags`
---
-
+);
+-- Create Tags Table
 CREATE TABLE `tags` (
-  `id` int NOT NULL,
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `tags`
---
-
-INSERT INTO `tags` (`id`, `name`) VALUES
-(1, 'Science'),
-(2, 'History'),
-(3, 'Literature'),
-(4, 'Technology'),
-(5, 'Art'),
-(6, 'Philosophy'),
-(7, 'Music'),
-(8, 'Travel'),
-(9, 'Food'),
-(10, 'Health'),
-(11, 'Fitness'),
-(12, 'Psychology'),
-(13, 'Education'),
-(14, 'Astronomy'),
-(15, 'Environment'),
-(16, 'Politics'),
-(17, 'Economics'),
-(18, 'Sociology'),
-(19, 'Languages'),
-(20, 'DIY');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
+);
+-- Create Users Table
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('reader','author','admin') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `wiki`
---
-
+-- CreateWikis Table
 CREATE TABLE `wiki` (
-  `id` int NOT NULL,
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `content` text NOT NULL,
   `creation_date` date NOT NULL,
   `status` enum('pending','approved','denied','archived','deleted') NOT NULL,
   `author_id` int NOT NULL,
-  `category_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wiki_tag`
---
-
+  `category_id` int NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user(id),
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+-- Create Pivot Wiki_tags Table
 CREATE TABLE `wiki_tag` (
   `tag_id` int NOT NULL,
-  `wiki_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `wiki_id` int NOT NULL,
+  FOREIGN KEY (tag_id) REFERENCES tags(id),
+  FOREIGN KEY (wiki_id) REFERENCES wiki(id)
+);
 
---
--- Indexes for dumped tables
---
+--Populate the Categories Table 
+INSERT INTO `category` (`name`) VALUES
+('Science'),
+('Literature'),
+('Technology'),
+('Art'),
+('Philosophy'),
+('Music'),
+('Food'),
+('Health'),
+('Fitness'),
+('Psychology'),
+('Education'),
+('Astronomy'),
+('Environment'),
+('Politics'),
+('Economics'),
+('Sociology'),
+('Languages'),
+('DIY');
 
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+--Populate the Tags Table 
+INSERT INTO `tags` (`name`) VALUES
+('Technology'),
+('Science'),
+('Programming'),
+('Art'),
+('Music'),
+('Nature'),
+('Travel'),
+('Food'),
+('Fitness'),
+('Health'),
+('Fashion'),
+('Design'),
+('Gaming'),
+('Books'),
+('Movies'),
+('Sports'),
+('Photography'),
+('History'),
+('Education'),
+('Business'),
+('Finance'),
+('Cooking'),
+('DIY'),
+('Hiking'),
+('Pets'),
+('Automobiles'),
+('Space'),
+('Fitness'),
+('Hobbies'),
+('Crafts'),
+('Entertainment'),
+('Anime'),
+('Memes'),
+('Politics'),
+('News'),
+('Podcasts'),
+('Comedy'),
+('Writing'),
+('Poetry'),
+('Mindfulness'),
+('Productivity'),
+('Coding'),
+('WebDevelopment'),
+('MobileApps'),
+('ArtificialIntelligence'),
+('VirtualReality'),
+('AugmentedReality'),
+('Blockchain'),
+('Cryptocurrency'),
+('Environment'),
+('FitnessMotivation'),
+('HealthyEating'),
+('Vegetarian'),
+('Vegan'),
+('WeightLoss'),
+('PhotographyLovers'),
+('Adventure'),
+('LuxuryTravel'),
+('StreetFood'),
+('HealthyRecipes'),
+('FashionStyle'),
+('InteriorDesign'),
+('GamingCommunity'),
+('ClassicMovies'),
+('Soccer'),
+('Basketball'),
+('Baseball'),
+('ScienceFiction'),
+('NaturePhotography'),
+('EducationForAll'),
+('SmallBusiness'),
+('Entrepreneurship'),
+('Investing'),
+('CookingAdventures'),
+('DIYProjects'),
+('CampingLife'),
+('PetLovers'),
+('CarEnthusiasts'),
+('SpaceExploration'),
+('YogaJourney'),
+('TechNews'),
+('Futurism'),
+('AIInnovation'),
+('VirtualRealityWorld'),
+('CryptocurrencyInvesting'),
+('EnvironmentalAwareness'),
+('WorkoutMotivation'),
+('BookLovers'),
+('MindfulLiving'),
+('WebDesign'),
+('MobileAppDevelopment'),
+('MachineLearning'),
+('VRGaming'),
+('BlockchainRevolution'),
+('HealthyLiving'),
 
---
--- Indexes for table `tags`
---
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `wiki`
---
-ALTER TABLE `wiki`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `WIKI_FK1` (`author_id`),
-  ADD KEY `WIKI_FK2` (`category_id`);
-
---
--- Indexes for table `wiki_tag`
---
-ALTER TABLE `wiki_tag`
-  ADD KEY `TAGS_FK_1` (`tag_id`),
-  ADD KEY `TAGS_FK_2` (`wiki_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tags`
---
-ALTER TABLE `tags`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `wiki`
---
-ALTER TABLE `wiki`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `wiki`
---
-ALTER TABLE `wiki`
-  ADD CONSTRAINT `WIKI_FK1` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `WIKI_FK2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `wiki_tag`
---
-ALTER TABLE `wiki_tag`
-  ADD CONSTRAINT `TAGS_FK_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `TAGS_FK_2` FOREIGN KEY (`wiki_id`) REFERENCES `wiki` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
